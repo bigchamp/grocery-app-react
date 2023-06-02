59b47486e2c51fe4ae9e125f8afbfc2044422bfc
Kai <kkarzhauov4@gmail.com>
2023-06-02 22:48:54 +0500

    Remove unnecessary files in base service

--- app/services/api/baseService.ts
+++ app/services/api/baseService.ts
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-// utils
-// import { remove, saveString } from './storage'
-
 const baseService = axios.create({
   baseURL: `${process.env.HOST}`,
   headers: {
@@ -12,18 +9,5 @@ const baseService = axios.create({
 })
 
 export const authHeaderName = 'Authorization'
-
-export const setAuthHeader = (token: string) => {
-  baseService.defaults.headers.common[authHeaderName] = `Bearer ${token}`
-}
-
-export const saveToken = async (token: string) => {
-//   await saveString('token', token)
-}
-
-export const clearTokens = async () => {
-  baseService.defaults.headers.common[authHeaderName] = ''
-//   await remove('token')
-}
 
 export default baseService
