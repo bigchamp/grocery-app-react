5bfa8fd7bcc7cbe067fe73514289c3086fe6a978
Kai <kkarzhauov4@gmail.com>
2023-06-02 22:42:55 +0500

    Center Loader

--- app/components/Loader/Loader.tsx
+++ app/components/Loader/Loader.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
-import { CircularProgress, CircularProgressProps } from '@mui/material';
+import { CircularProgress, CircularProgressProps, Container } from '@mui/material';
 
-export const Loader = (props: CircularProgressProps) => <CircularProgress {...props} />
+export const Loader = (props: CircularProgressProps) => <Container sx={containerStyle}>
+    <CircularProgress sx={{my: 2}} {...props} />
+</Container>
+
+const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center'
+}
