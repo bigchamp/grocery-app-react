894a2278c0c945c4fe7556ec3ea33e82d48a06f1
Kai <kkarzhauov4@gmail.com>
2023-06-08 02:22:04 +0500

    Edit next config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        HOSTNAME: 'localhost',
-        PORT: '3001',
-        HOST: 'http://localhost:3001'
-    },
-    async redirects() {
-        return [
-          {
-            source: '/home',
-            destination: '/',
-            permanent: true,
-          },
-        ]
-      },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
